72fb1f3d629b7c827a81f28910332539ac9aa7d7
rigalsan09 <rigallegos09@gmail.com>
2024-02-20 18:58:00 -0800

    Added table and caption

---
+++ javascript.js
@@ -0,0 +1,30 @@
+//foundations part 1
+// let a = 10;
+// console.log(9 * a);
+
+// let b = 7 * a;
+
+// console.log(b);
+
+// const max = 57;
+
+// let actaul = max - 13;
+
+// let percetange = actaul /max;
+
+
+// let x = (0.2 * 10 + 0.1 * 10) / 10;
+
+// console.log(x);
+
+// let x = 10;
+// let y = 20;
+// let z = "The result is " + x + y;
+
+// console.log(z);
+
+
+let x = NaN;
+let y = "5";
+let z = x + y;
+
